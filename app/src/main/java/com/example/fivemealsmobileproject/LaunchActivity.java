fa5150f48e5fb960939e4933233503b216ed200f
David Capa <capaseitadavid@gmail.com>
2022-05-30 23:42:05 +0100

    Login Layouts

--- app/src/main/java/com/example/fivemealsmobileproject/LaunchActivity.java
+++ app/src/main/java/com/example/fivemealsmobileproject/LaunchActivity.java
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+
+import com.example.fivemealsmobileproject.login.PreLoginActivity;
 
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch);
+
+        SystemClock.sleep(2000);
+        Intent intent = new Intent(this, PreLoginActivity.class);
+        startActivity(intent);
     }
 
-    //TODO-
 
-    /*
-    public static void startActivity(Context context){
-        Intent intent = new Intent(context, LaunchActivity.class);
-        context.startActivity(intent);
-    }
-    */
+
+
+
+
+
+
+
 
 }
---
+++ app/src/main/java/com/example/fivemealsmobileproject/login/PreLoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.fivemealsmobileproject.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.fivemealsmobileproject.LaunchActivity;
+import com.example.fivemealsmobileproject.R;
+
+public class PreLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_login);
+
+    }
+
+
+
+}
