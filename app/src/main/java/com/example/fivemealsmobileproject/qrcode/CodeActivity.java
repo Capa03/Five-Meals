2906931bc2a6062445d6c49519064462565b122a
LuisAntunes26 <antunes2603@gmail.com>
2022-06-07 13:01:02 +0100

    - Debug Toasts delete - Comment debug Table Database

--- app/src/main/java/com/example/fivemealsmobileproject/qrcode/CodeActivity.java
+++ app/src/main/java/com/example/fivemealsmobileproject/qrcode/CodeActivity.java
@@ -33,7 +33,6 @@ public class CodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_input);
-        Toast.makeText(this, "QrCode", Toast.LENGTH_SHORT).show();
         cacheViews();
         Context context = this;
        
