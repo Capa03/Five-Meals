1a2e056808a715d1ddc367f8f7f1b7ade38576b1
David Capa <capaseitadavid@gmail.com>
2022-06-09 13:20:58 +0100

    Account Fragment layout and get username and image with glide

--- app/src/main/java/com/example/fivemealsmobileproject/account/AccountFragment.java
+++ app/src/main/java/com/example/fivemealsmobileproject/account/AccountFragment.java
@@ -1,19 +1,40 @@
 package com.example.fivemealsmobileproject.account;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContentInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.fivemealsmobileproject.R;
+import com.example.fivemealsmobileproject.database.AppDataBase;
+import com.example.fivemealsmobileproject.database.User;
+import com.example.fivemealsmobileproject.login.SessionManager;
+
+import java.util.List;
+import java.util.Random;
 
 
 public class AccountFragment extends Fragment {
+
+    private Context context;
+
+    private static String[] profilePictures = new String[] {
+            "http://tcap.pbworks.com/f/1435170280/myAvatar.png",
+            "https://www.f6s.com/content-resource/profiles/3072512_original.jpg",
+            "https://layogroup.net/wp-content/uploads/2019/07/522569-1eWJyL1490726864.png",
+            "https://cdn.dribbble.com/users/2364329/screenshots/5930135/aa.jpg"
+    };
+
 
 
     public AccountFragment() {
@@ -31,11 +52,19 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        this.context = container.getContext();
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 
+    private Random random = new Random();
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ImageView imageView = view.findViewById(R.id.imageViewAccountImage);
+        TextView username = view.findViewById(R.id.textViewAccountUsername);
+        Glide.with(this.context).load(profilePictures[random.nextInt(profilePictures.length)]).into(imageView);
+        String user = SessionManager.getActiveSession(this.context);
+        username.setText(user);
+        //TODO Trazer info completo do user
     }
 }
